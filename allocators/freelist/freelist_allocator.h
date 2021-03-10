cf0259df8d1a389d6c7ffa828ae5e4306549805a
rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-03-10 20:04:08 +0000

    freelist allocator

---
+++ allocators/freelist/freelist_allocator.h
@@ -0,0 +1,261 @@
+#include <unistd.h>
+#include <cstdint>
+#include <utility>
+#include <list>
+
+namespace isa
+{
+	class freelist_allocator
+	{
+		using word_t = std::intptr_t;
+
+		struct free_block
+		{
+			free_block* next;
+			free_block* prev;
+			size_t sz;
+		};
+
+		struct block
+		{
+			block* next;
+			block* prev;
+			block* next_free;
+			block* prev_free;
+			size_t sz;
+			bool used;
+			word_t data[1];
+		};
+
+
+		block* heap_head = nullptr;
+		block* heap_tail = nullptr;
+		block* free_head = nullptr;
+		block* free_tail = nullptr;
+
+	#ifdef DEBUG
+	public:
+	#endif
+		block* search_start = nullptr;
+
+		inline block* get_header(word_t* data) const noexcept
+		{
+			return (block*) ( (char*)data - sizeof(block) + sizeof(std::declval<block>().data));
+		}
+	#ifdef DEBUG
+	private:
+	#endif
+
+		inline size_t align(size_t n) const noexcept
+		{
+			return (n + sizeof(word_t) - 1) & ~(sizeof(word_t) - 1);
+		}
+
+		inline size_t alloc_size(size_t n) const noexcept
+		{
+			// block already contains 1 word inside it, so we allocate 1 block + n - word bytes;
+			return sizeof(block) + n - sizeof(std::declval<block>().data);
+		}
+
+		block* request_from_os(size_t n) const noexcept
+		{
+			block* blk = (block*) sbrk(0); // get current brk pointer position
+
+			if(sbrk(alloc_size(n)) == (void*)-1) // (void*)-1 = maximum possible integer value of a pointer. this means out of memory
+			{
+				return nullptr;
+			}
+			return blk;
+		}
+
+		inline bool can_split(block* blk, size_t n) const noexcept
+		{
+			return (blk->sz) > alloc_size(n); 
+		}
+
+		block* split(block* blk, size_t n) 
+		{
+			if(can_split(blk, n))
+			{
+				block* new_blk = (block*)	((char*)blk + alloc_size(n));
+				new_blk->sz = blk->sz - alloc_size(n);
+
+				new_blk->next = blk->next;
+				new_blk->next->prev = new_blk;
+				blk->next = new_blk;
+				new_blk->prev = blk;
+
+				new_blk->next_free = blk->next_free;
+				new_blk->next_free->prev_free = new_blk;
+				blk->next_free = new_blk;
+				new_blk->prev_free = blk;
+			}
+
+			blk->sz = n;
+
+			return blk;
+		}
+
+		inline bool not_tail(block* blk) const noexcept
+		{
+			return blk != heap_tail;
+		}
+
+		inline bool not_head(block* blk) const noexcept
+		{
+			return blk != heap_head;
+		}
+
+		void merge_free(block* blk)
+		{
+			
+		}
+
+		block* try_merge(block* blk)
+		{
+			block* next = blk->next;
+			block* next_free = blk->next_free;
+		
+			if(not_head(blk) && !blk->prev->used)
+			{
+				blk = try_merge(blk->prev);
+			}
+
+			if(not_tail(blk) && !next->used)
+			{
+				remove_free_block(next);
+
+				blk->sz += alloc_size(next->sz);
+				blk->next = next->next;
+				next->next->prev = blk;
+
+				//add_free_block(blk);
+			}
+
+			return blk;
+		}
+
+		void remove_free_block(block* blk)
+		{
+			if(blk == free_tail && free_tail == free_head)
+			{
+				free_head = free_tail = nullptr;
+				search_start = free_head;
+			}
+			else
+			{
+				blk->prev_free->next_free = blk->next_free;
+				blk->next_free->prev_free = blk->prev_free;
+				search_start = blk->next_free;
+			}
+			
+			blk->used = true;
+		}
+
+		void add_free_block(block* blk)
+		{
+			if(free_head == nullptr)
+			{
+				free_head = blk;
+				free_head->next_free = free_head->prev_free = blk;
+				search_start = free_head;
+			}
+			else
+			{
+				free_tail->next_free = blk;
+				blk->prev_free = free_tail;
+			}
+			free_tail = blk;
+			free_tail->next_free = free_head;
+			free_head->prev_free = free_tail;
+			blk->used = false;
+		}
+
+		block* find_block(size_t n)
+		{
+			block* curr = search_start;
+
+			if(curr == nullptr)
+				return nullptr;
+
+			do
+			{
+				if(curr->sz >= n)
+				{
+					block* res = split(curr, n);
+					remove_free_block(res);
+					return res;
+				}
+				curr = curr->next_free;
+			} while(curr != free_head);
+
+			return nullptr;
+		}
+
+		void init()
+		{
+			reset_brk();
+		}
+
+		void reset_brk() noexcept
+		{
+			if(heap_head == nullptr)
+				return;
+
+			brk(heap_head);
+			heap_head = nullptr;
+			heap_tail = nullptr;
+			free_head = nullptr;
+			free_tail = nullptr;
+		}
+
+	public:
+
+		freelist_allocator() 
+		{
+			init();
+		}
+
+		word_t* alloc(size_t n) noexcept
+		{
+			n = align(n);
+
+			if(block* blk = find_block(n))
+			{
+				return blk->data;
+			}
+			
+			block* block = request_from_os(n);
+			block->sz = n;
+			block->used = true;
+
+			if(heap_head == nullptr) // very first allocation
+			{
+				heap_head = block;
+				heap_head->prev = heap_head->next = block;
+			}
+			if(heap_tail != nullptr)
+			{
+				heap_tail->next = block;
+				block->prev = heap_tail;
+			}
+
+			heap_tail = block;
+			heap_tail->next = heap_head;
+			heap_head->prev = heap_tail;
+
+			// return pointer to memory allocated for user;
+			return block->data;
+		}
+
+		void free(word_t* data) {
+ 			block* block = get_header(data);
+			block->used = false;
+			add_free_block(block);
+			try_merge(block);
+		}
+	};
+
+	using falloc = freelist_allocator;
+
+}
---
+++ allocators/freelist/tests.cpp
@@ -0,0 +1,86 @@
+#define DEBUG
+
+#include "freelist_allocator.h"
+#include <cassert>
+#include <cstdio>
+
+using word_t = std::intptr_t;
+
+int main()
+{
+	isa::freelist_allocator a;
+
+	auto p1 = a.alloc(3);
+	auto p1b = a.get_header(p1);
+	assert(p1b->sz == sizeof(word_t));
+
+	auto p2 = a.alloc(8);
+  	auto p2b = a.get_header(p2);
+  	assert(p2b->sz == 8);
+
+	a.free(p2);
+	assert(p2b->used == false);
+
+	auto p3 = a.alloc(8);
+	auto p3b = a.get_header(p3);
+	assert(p3b->sz == 8);
+	assert(p3b == p2b); // reused
+
+	isa::freelist_allocator a2;
+	 
+	a2.alloc(8);
+	a2.alloc(8);
+	a2.alloc(8);
+	 
+	auto o1 = a2.alloc(16);
+	auto o2 = a2.alloc(16);
+	 
+	// must merge
+	a2.free(o1);
+	a2.free(o2);
+	 
+	// must split previously merged block.
+	auto o3 = a2.alloc(16);
+	 
+	assert(o3 == o1); // reused
+	assert(a2.search_start == a2.get_header(o2)); // search start set to last successfuly found block
+	 
+	auto o4 = a2.alloc(16);
+	assert(o4 == o2); // reused
+
+	isa::freelist_allocator a3;
+	a3.alloc(8);
+
+	auto z1 = a3.alloc(64);
+	auto z2 = a3.alloc(16);
+	a3.alloc(8);
+
+	a3.free(z2);
+	a3.free(z1); // 64 and 16 must merge into 104 
+
+	auto z3 = a3.alloc(16);
+	// split 104-byte block. new 16 byte block is taken from begining of 104 block.
+	assert(a3.get_header(z3) == a3.get_header(z1)); 
+
+	auto z4 = a3.alloc(16); // split 64-byte block z1
+	assert((char*)a3.get_header(z4) == (char*) (z3 + 2)); // z4 block start right where z3 data ends;
+
+
+	isa::freelist_allocator a4;
+	
+	a4.alloc(8);
+
+	auto m1 = a4.alloc(64);
+	auto m2 = a4.alloc(64);
+
+	a4.free(m2);
+	a4.free(m1);
+
+	auto m3 = a4.alloc(104);
+	assert(m3 == m1);
+
+	auto m4 = a4.alloc(24);
+	assert((char*)m4 == (char*)m3 + 104 - sizeof(word_t) + 56); // 56 is sizeof(block)
+
+	std::puts("\nAll assertions passed!\n");
+}
