7796672273d56e0d423f00ac55d96b7d8d9ae502
Rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-03-18 17:22:48 +0600

    test gcc std::list

---
+++ data structures/list/learn_gcc/tests/front.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Isa on 3/17/21.
+//
+
+#include "test_utils.h"
+
+#define tag "[front]"
+
+
+TEST_CASE("front", tag)
+{
+	gcc_list<string> act(input_string.begin(), input_string.end());
+	gcc_list<string> exp(input_string.begin(), input_string.end());
+
+	REQUIRE(act.front() == exp.front());
+
+	initializer_list<int> rand_int_inp = rand_ints;
+	gcc_list<int> act2 = rand_int_inp;
+	std_list<int> exp2 = rand_int_inp;
+
+	REQUIRE(act2.front() == exp2.front());
+}
