5d1484c820406682e5d9d273b00c663db859b217
Rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-03-19 00:31:00 +0600

    test gcc std::list

---
+++ data structures/list/learn_gcc/tests/erase.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Isa on 3/19/21.
+//
+
+#include "test_utils.h"
+
+#define tag "[erase]"
+
+
+TEST_CASE("erase", tag)
+{
+
+}
+
+TEST_CASE("range erase", tag "[range erase]")
+{
+
+}
