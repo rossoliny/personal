3181ae313181c1c9a3383c2fb8f5cd3b0f4f8963
Rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-03-18 21:04:48 +0600

    test gcc std::list

--- data structures/list/learn_gcc/tests/front.cpp
+++ data structures/list/learn_gcc/tests/front.cpp
@@ -20,3 +20,17 @@ TEST_CASE("front", tag)
 
 	REQUIRE(act2.front() == exp2.front());
 }
+
+TEST_CASE("const front", tag)
+{
+	gcc_list<string>const act(input_string.begin(), input_string.end());
+	gcc_list<string>const exp(input_string.begin(), input_string.end());
+
+	REQUIRE(act.front() == exp.front());
+
+	initializer_list<int> rand_int_inp = rand_ints;
+	const gcc_list<int> act2 = rand_int_inp;
+	const std_list<int> exp2 = rand_int_inp;
+
+	REQUIRE(act2.front() == exp2.front());
+}
