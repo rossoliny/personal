b37a1418fcbe20393cc3f21c87acf32e022c9669
Rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-03-18 21:09:36 +0600

    test gcc std::list

--- data structures/list/learn_gcc/tests/front_and_back.cpp
+++ data structures/list/learn_gcc/tests/front_and_back.cpp
@@ -34,3 +34,36 @@ TEST_CASE("const front", tag)
 
 	REQUIRE(act2.front() == exp2.front());
 }
+
+
+#undef tag
+#define tag "[back]"
+
+
+TEST_CASE("back", tag)
+{
+	gcc_list<string> act(input_string.begin(), input_string.end());
+	gcc_list<string> exp(input_string.begin(), input_string.end());
+
+	REQUIRE(act.back() == exp.back());
+
+	initializer_list<int> rand_int_inp = rand_ints;
+	gcc_list<int> act2 = rand_int_inp;
+	std_list<int> exp2 = rand_int_inp;
+
+	REQUIRE(act2.back() == exp2.back());
+}
+
+TEST_CASE("const back", tag)
+{
+	gcc_list<string>const act(input_string.begin(), input_string.end());
+	gcc_list<string>const exp(input_string.begin(), input_string.end());
+
+	REQUIRE(act.back() == exp.back());
+
+	initializer_list<int> rand_int_inp = rand_ints;
+	const gcc_list<int> act2 = rand_int_inp;
+	const std_list<int> exp2 = rand_int_inp;
+
+	REQUIRE(act2.back() == exp2.back());
+}
