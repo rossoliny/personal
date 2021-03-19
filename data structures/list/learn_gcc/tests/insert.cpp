9905a4190fb295ae656e7fecc977ed237203a367
Rossolinyisanautist <dzhumabaev_i@auca.kg>
2021-03-19 18:20:38 +0600

    test gcc std::list

--- data structures/list/learn_gcc/tests/insert.cpp
+++ data structures/list/learn_gcc/tests/insert.cpp
@@ -9,7 +9,17 @@
 
 TEST_CASE("insert single element", tag)
 {
+	CREATE_LISTS_OF_INT(l);
 
+	auto pos1 = gcc_l.begin();
+	auto pos2 = std_l.begin();
+	advance(pos1, rand() % gcc_l.size() + 1);
+	advance(pos2, rand() % std_l.size() + 1);
+
+	int val = rand();
+
+	gcc_l.insert(pos1, val);
+	std_l.insert(pos2, val);
 }
 
 TEST_CASE("fill insert", tag)
@@ -29,5 +39,5 @@ TEST_CASE("move insert", tag)
 
 TEST_CASE("initializer list insert", tag)
 {
-	
+
 }
