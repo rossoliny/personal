589b703ee01a0ff724b189d97553a6be89e8449e
Isa Dzhumabaev <dzhumabaev_i@auca.kg>
2021-05-05 14:45:12 +0600

    quicksort

--- qsort/qsort.cpp
+++ qsort/qsort.cpp
@@ -1,10 +1,62 @@
 #include <cstdlib>
 
-typedef int (*cmp)(const void*, const void*);
-void qsort(void *base, size_t nitems, size_t size, cmp) {
-	// TODO: implement
+
+#include "stdio.h"
+#include "time.h"
+#include "stdlib.h"
+
+int partition (int* arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+    }
+    int t = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = t;
+
+    return (i + 1);
 }
 
-int main() {
+void quickSort(int* arr, int low, int high)
+{
+    if (low < high)
+    {
 
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main(int argc, char** argv) {
+
+    int sz = -1;
+    printf("Enter size of array:\n");
+    scanf("%d", &sz);
+    int arr[sz];
+
+    srand(time(NULL));
+    for (int i = 0; i < sz; ++i)
+    {
+    	arr[i] = rand() % (sz * 20);
+    }
+
+    clock_t begin = clock();
+    quickSort(arr, 0, sz - 1);
+    clock_t end = clock();
+
+    printf("Time taken: %f seconds\n", ((double) (end - begin)) / CLOCKS_PER_SEC);
+
+    return 0;
 }
