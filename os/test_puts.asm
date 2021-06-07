afdf64a1d34091e4d3373b0fb6cb54f5f8c2a94b
Isa Dzhumabaev <isa@congruity360.com>
2021-06-07 19:04:37 +0600

    asm puts

---
+++ os/test_puts.asm
@@ -0,0 +1,23 @@
+
+[org 0x7c00]
+
+mov bx, _HELLO_MSG_
+call _puts 
+
+mov bx, _GOODBYE_MSG_
+call _puts
+
+idle:
+	jmp $
+
+%include "./include/_puts.asm"
+
+_HELLO_MSG_:
+	db 'Hello, World!', 0xA, 0xD, 0
+
+_GOODBYE_MSG_:
+	db 'Goodbye', 0xA, 0xD, 0
+
+times 510-($-$$) db 0
+dw 0xaa55
+
